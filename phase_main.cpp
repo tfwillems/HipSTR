723e58bad77a5ee31438da4ee64f7f8ab13c5178
Thomas Willems <twillems@club.wi.mit.edu>
2015-03-05 20:00:47 -0500

    Added command line options to specify the input files

--- phase_main.cpp
+++ phase_main.cpp
@@ -4,9 +4,12 @@
 #include <dai/bp.h>
 #include <dai/decmap.h>
 
+#include <getopt.h>
 #include <fstream>
 #include <iostream>
 #include <string>
+
+#include "error.h"
 
 class Node_GT {
 public:
@@ -30,6 +33,7 @@ public:
 void read_node_genotypes(std::string input_file, std::vector<Node_GT>& node_gts){
   int node_id, genotype;
   std::ifstream input(input_file);
+  if(!input.is_open()) printErrorAndDie("Failed to open node genotype file");
   while (input >> node_id >> genotype)
     node_gts.push_back(Node_GT(node_id, genotype));
   input.close();
@@ -39,6 +43,7 @@ void read_pair_genotypes(std::string input_file, std::vector<Node_Pair_GT>& pair
   int node_id_1, node_id_2;
   int node_gt_a, node_gt_b;
   std::ifstream input(input_file);
+  if(!input.is_open()) printErrorAndDie("Failed to open pair information file");
   while (input >> node_id_1 >> node_id_2 >> node_gt_a >> node_gt_b)
     pair_gts.push_back(Node_Pair_GT(node_id_1, node_id_2, node_gt_a, node_gt_b));
   input.close();
@@ -47,6 +52,7 @@ void read_pair_genotypes(std::string input_file, std::vector<Node_Pair_GT>& pair
 void read_node_ids(std::string input_file, std::vector<int>& node_ids){
   int node_id;
   std::ifstream input(input_file);
+  if (!input.is_open()) printErrorAndDie("Failed to open node id file");
   while (input >> node_id)
     node_ids.push_back(node_id);
   input.close();
@@ -57,13 +63,68 @@ void fix_genotypes(std::vector< std::pair<int,int> >& gt_info, dai::FactorGraph&
     fg.clamp(gt_info[i].first, gt_info[i].second, true);
 }
 
+void parse_command_line_args(int argc, char** argv, 
+			     std::string& factor_graph_file, std::string& pair_file, std::string& id_file){
+  if (argc == 1){
+    std::cerr << "Usage: Phaser --factor-graph <fg.dai> [--pair-file <pairs.txt>] [--id-file <ids.txt>]" << "\n"
+	      << "\t" << "--factor-graph <fg.dai>        "  << "\t" << "Factor graph file in libDAI format" << "\n"
+	      << "\t" << "--pair-file    <pairs.txt>     "  << "\t" << "File containing node-pairing information, one pair per line (id_1 id_2 gt_a gt_b)" << "\n"
+	      << "\t" << "                               "  << "\t" << "Will print the phasing posterior probability in the format id_1 id_2 P[GT(id_1)=gt_a] P[GT(id_1)=gt_b]" << "\n"
+	      << "\t" << "--id-file      <ids.txt>       "  << "\t" << "File containing node id information, one id per line." << "\n"
+	      << "\t" << "                               "  << "\t" << "Will print the posterior probabilities for each included id, one id per line" << "\n"
+	      << "\n";
+    exit(0);
+  }
+ 
+  static struct option long_options[] = {
+    {"factor-graph",    required_argument, 0, 'f'},
+    {"id-file",         required_argument, 0, 'i'},
+    {"pair-file",       required_argument, 0, 'p'},
+    {0, 0, 0, 0}
+  };
+
+  int c;
+  while (true){
+    int option_index = 0;
+    c = getopt_long(argc, argv, "f:i:p:", long_options, &option_index);
+    if (c == -1)
+      break;
+
+    switch(c){
+    case 0:
+      break;
+    case 'f':
+      factor_graph_file = std::string(optarg);
+      break;
+    case 'i':
+      id_file = std::string(optarg);
+      break;
+    case 'p':
+      pair_file = std::string(optarg);
+      break;
+    case '?':
+      printErrorAndDie("Unrecognized command line option");
+      break;
+    default:
+      abort();
+      break;
+    }
+  }
+}
+
 int main(int argc, char *argv[]) {
+  std::string factor_graph_file="", pair_file="", id_file="";
+  parse_command_line_args(argc, argv, factor_graph_file, pair_file, id_file);
+  if (factor_graph_file.empty())
+    1;
+
+
   dai::FactorGraph fg;
   std::cerr << "Reading factor graph" << std::endl;
-  fg.ReadFromFile(argv[1]);
+  fg.ReadFromFile(factor_graph_file.c_str());
 
   size_t maxiter    = 2000;
-  dai::Real tol     = 1e-9;
+  dai::Real tol     = 1e-5;
   size_t verb       = 3;
   dai::Real damping = 0.01;
 
@@ -74,27 +135,31 @@ int main(int argc, char *argv[]) {
   opts.set("damping", damping);   // Degree to which messages should be damped 
 
   std::vector<Node_Pair_GT> pair_gts;
-  read_pair_genotypes(argv[2], pair_gts);
+  if(!pair_file.empty())
+    read_pair_genotypes(pair_file, pair_gts);
 
   std::vector<int> node_ids;
-  read_node_ids(argv[3], node_ids);
+  if (!id_file.empty())
+    read_node_ids(id_file, node_ids);
   
   dai::BP bp(fg, opts("updates", std::string("SEQRND"))("logdomain",true));
   bp.init();
   bp.run();
 
-
-  for (unsigned int i = 0; i < pair_gts.size(); ++i){
-    std::cout << pair_gts[i].id_1 << "\t" << pair_gts[i].id_2 << "\t"
-	      << pair_gts[i].gt_a << "\t" << pair_gts[i].gt_b << "\t" 
-	      << bp.belief(bp.var(pair_gts[i].id_1))[pair_gts[i].gt_a] << "\t" 
-	      << bp.belief(bp.var(pair_gts[i].id_1))[pair_gts[i].gt_b] << std::endl;
+  if(!pair_file.empty()){
+    for (unsigned int i = 0; i < pair_gts.size(); ++i){
+      std::cout << pair_gts[i].id_1 << "\t" << pair_gts[i].id_2 << "\t"
+		<< pair_gts[i].gt_a << "\t" << pair_gts[i].gt_b << "\t" 
+		<< bp.belief(bp.var(pair_gts[i].id_1))[pair_gts[i].gt_a] << "\t" 
+		<< bp.belief(bp.var(pair_gts[i].id_1))[pair_gts[i].gt_b] << std::endl;
+    }
   }
   
-  for (unsigned int i = 0; i < node_ids.size(); ++i){
-    std::cout << node_ids[i] << bp.belief(bp.var(node_ids[i])) << std::endl;
+  if(!id_file.empty()){
+      for (unsigned int i = 0; i < node_ids.size(); ++i){
+	std::cout << node_ids[i] << bp.belief(bp.var(node_ids[i])) << std::endl;
+      }
   }
-
-  
+      
   return 0;
 }
