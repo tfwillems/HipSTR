69ac94003efd9409e44b434329fa76021a02190f
Thomas Willems <twillems@club.wi.mit.edu>
2015-03-17 13:33:57 -0400

    Stutter model now handles both in-frame and out-of-frame mutations

--- stutter_model.cpp
+++ stutter_model.cpp
@@ -1,7 +1,9 @@
 #include "stutter_model.h"
 
 std::ostream& operator<< (std::ostream &out, StutterModel& model){
-  out << "(Stutter Model: P_GEOM=" << model.p_geom_ << ", P_DOWN=" << model.p_down_ << ", P_UP=" << model.p_up_ << std::endl; 
+  out << "(Stutter Model:"  << "\n"
+      << "IN_FRAME [P_GEOM(rep) =" << model.in_geom_  << ", P_DOWN =" << model.in_down_  << ", P_UP =" << model.in_up_  << "]" << "\n"
+      << "OUT_FRAME[P_GEOM(bp)  =" << model.out_geom_ << ", P_DOWN =" << model.out_down_ << ", P_UP =" << model.out_up_ << "]" << std::endl;
   return out;
 }
 
--- stutter_model.h
+++ stutter_model.h
@@ -7,33 +7,66 @@
 
 class StutterModel {
  private:
-  double p_geom_, p_up_, p_down_;
-  double log_nostep_, log_step_;
-  double log_up_, log_down_, log_equal_;
+  // Parameters for in-frame mutations. Geometric parameter in units of repeats
+  double in_geom_, in_up_, in_down_;
+  double in_log_nostep_, in_log_step_;
+  double in_log_up_, in_log_down_, in_log_equal_;
+
+  // Parameters for out-of-frame mutations. Geometric parameter in units of base pairs
+  double out_geom_, out_up_, out_down_;
+  double out_log_nostep_, out_log_step_;
+  double out_log_up_, out_log_down_;
+  
+  int motif_len_;
   
  public:
-  StutterModel(double p_geom, double p_up, double p_down){
-    assert(p_geom > 0 && p_geom < 1.0);
-    assert(p_up > 0.0 && p_down > 0.0 && p_up+p_down < 1.0);
-    p_geom_     = p_geom; 
-    log_step_   = log(1-p_geom); 
-    log_nostep_ = log(p_geom);
-    p_up_       = p_up; 
-    log_up_     = log(p_up);
-    p_down_     = p_down;
-    log_down_   = log(p_down);
-    log_equal_  = log(1.0-p_up-p_down);
+  StutterModel(double inframe_geom,  double inframe_up,  double inframe_down, 
+	       double outframe_geom, double outframe_up, double outframe_down, int motif_len){
+    assert(inframe_geom > 0 && inframe_geom < 1.0);
+    assert(inframe_up > 0.0 && inframe_down > 0.0 && inframe_up+inframe_down < 1.0);
+    assert(outframe_geom > 0 && outframe_geom < 1.0);
+    assert(outframe_up > 0.0 && outframe_down > 0.0 && outframe_up+outframe_down < 1.0);
+
+    in_geom_        = inframe_geom;
+    in_log_step_    = log(1-inframe_geom);
+    in_log_nostep_  = log(inframe_geom);
+    in_up_          = inframe_up;
+    in_log_up_      = log(inframe_up);
+    in_down_        = inframe_down;
+    in_log_down_    = log(inframe_down);
+    in_log_equal_   = log(1-inframe_up-inframe_down);
+
+    out_geom_       = outframe_geom;
+    out_log_step_   = log(1-outframe_geom);
+    out_log_nostep_ = log(outframe_geom);
+    out_up_         = outframe_up;
+    out_log_up_     = log(outframe_up);
+    out_down_       = outframe_down;
+    out_log_down_   = log(outframe_down);
+
+    motif_len_      = motif_len;
  }
 
   friend std::ostream& operator<< (std::ostream &out, StutterModel& model);
 
-  inline double calc_log_stutter(int sample_repeats, int read_repeats){
-    if (read_repeats == sample_repeats)
-      return log_equal_;
-    if (read_repeats < sample_repeats)
-      return log_down_ + log_nostep_ + log_step_*(sample_repeats-read_repeats);
-    else
-      return log_up_   + log_nostep_ + log_step_*(read_repeats-sample_repeats);
+  double calc_log_stutter(int sample_bps, int read_bps){
+    int bp_diff = read_bps - sample_bps;
+    if (bp_diff % motif_len_ != 0){
+      int eff_diff = bp_diff - (bp_diff/motif_len_);
+      if (eff_diff < 0)
+	return out_log_down_ + out_log_nostep_ + out_log_step_*(-eff_diff-1);
+      else
+	return out_log_up_   + out_log_nostep_ + out_log_step_*(eff_diff-1);
+    }
+    else {
+      int rep_diff = bp_diff/motif_len_;
+      if (rep_diff == 0)
+	return in_log_equal_;
+      if (rep_diff < 0)
+	return in_log_down_ + in_log_nostep_ + in_log_step_*(-rep_diff-1);
+      else
+	return in_log_up_   + in_log_nostep_ + in_log_step_*(rep_diff-1);
+    }
   }
 };
 
