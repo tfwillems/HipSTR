4aa40c305e3dc5a6cab425587ba161597fc21576
Thomas Willems <twillems@club.wi.mit.edu>
2015-04-09 21:32:25 -0400

    Slight code refactoring

--- stutter_model.cpp
+++ stutter_model.cpp
@@ -1,9 +1,8 @@
 #include "stutter_model.h"
 
 std::ostream& operator<< (std::ostream &out, StutterModel& model){
-  out << "(Stutter Model:"  << "\n"
-      << "IN_FRAME [P_GEOM(rep) =" << model.in_geom_  << ", P_DOWN =" << model.in_down_  << ", P_UP =" << model.in_up_  << "]" << "\n"
-      << "OUT_FRAME[P_GEOM(bp)  =" << model.out_geom_ << ", P_DOWN =" << model.out_down_ << ", P_UP =" << model.out_up_ << "]" << std::endl;
+  out << "IN_FRAME [P_GEOM(rep)=" << model.in_geom_  << ", P_DOWN=" << model.in_down_  << ", P_UP=" << model.in_up_  << "]" << "\n"
+      << "OUT_FRAME[P_GEOM(bp) =" << model.out_geom_ << ", P_DOWN=" << model.out_down_ << ", P_UP=" << model.out_up_ << "]" << std::endl;
   return out;
 }
 
--- stutter_model.h
+++ stutter_model.h
@@ -14,7 +14,10 @@ class StutterModel {
   // Parameters for in-frame mutations. Geometric parameter in units of repeats
   double in_geom_, in_up_, in_down_;
   double in_log_nostep_, in_log_step_;
-  double in_log_up_, in_log_down_, in_log_equal_;
+  double in_log_up_, in_log_down_;
+
+  // Parameter for no stutter change
+  double log_equal_;
 
   // Parameters for out-of-frame mutations. Geometric parameter in units of base pairs
   double out_geom_, out_up_, out_down_;
@@ -42,10 +45,11 @@ class StutterModel {
  public:
   StutterModel(double inframe_geom,  double inframe_up,  double inframe_down, 
 	       double outframe_geom, double outframe_up, double outframe_down, int motif_len){
-    assert(inframe_geom > 0 && inframe_geom < 1.0);
-    assert(inframe_up > 0.0 && inframe_down > 0.0 && inframe_up+inframe_down < 1.0);
-    assert(outframe_geom > 0 && outframe_geom < 1.0);
-    assert(outframe_up > 0.0 && outframe_down > 0.0 && outframe_up+outframe_down < 1.0);
+    assert(inframe_geom  > 0.0 && inframe_geom  < 1.0);
+    assert(outframe_geom > 0.0 && outframe_geom < 1.0);
+    assert(inframe_up    > 0.0 && inframe_down  > 0.0);
+    assert(outframe_up   > 0.0 && outframe_down > 0.0);
+    assert(inframe_up + inframe_down + outframe_up + outframe_down < 1.0);
 
     in_geom_        = inframe_geom;
     in_log_step_    = log(1-inframe_geom);
@@ -54,7 +58,6 @@ class StutterModel {
     in_log_up_      = log(inframe_up);
     in_down_        = inframe_down;
     in_log_down_    = log(inframe_down);
-    in_log_equal_   = log(1-inframe_up-inframe_down);
 
     out_geom_       = outframe_geom;
     out_log_step_   = log(1-outframe_geom);
@@ -64,6 +67,7 @@ class StutterModel {
     out_down_       = outframe_down;
     out_log_down_   = log(outframe_down);
 
+    log_equal_      = log(1-inframe_up-inframe_down-outframe_up-outframe_down);
     motif_len_      = motif_len;
  }
 
@@ -72,7 +76,7 @@ class StutterModel {
   double get_parameter(bool in_frame, char parameter){
     switch(parameter){
     case 'U':
-      return in_frame ? in_up_ : out_up_;
+      return in_frame ? in_up_   : out_up_;
     case 'D':
       return in_frame ? in_down_ : out_down_;
     case 'P':
@@ -88,23 +92,28 @@ class StutterModel {
     Returns the read's log-likeliood given that it contains exactly the provided number of base pairs
   */
   double log_stutter_pmf(int sample_bps, int read_bps){
+    double log_pmf;
     int bp_diff = read_bps - sample_bps;
     if (bp_diff % motif_len_ != 0){
       int eff_diff = bp_diff - (bp_diff/motif_len_);
       if (eff_diff < 0)
-	return out_log_down_ + out_log_nostep_ + out_log_step_*(-eff_diff-1);
+	log_pmf = out_log_down_ + out_log_nostep_ + out_log_step_*(-eff_diff-1);
       else
-	return out_log_up_   + out_log_nostep_ + out_log_step_*(eff_diff-1);
+	log_pmf = out_log_up_   + out_log_nostep_ + out_log_step_*(eff_diff-1);
     }
     else {
       int rep_diff = bp_diff/motif_len_;
       if (rep_diff == 0)
-	return in_log_equal_;
-      if (rep_diff < 0)
-	return in_log_down_ + in_log_nostep_ + in_log_step_*(-rep_diff-1);
-      else
-	return in_log_up_   + in_log_nostep_ + in_log_step_*(rep_diff-1);
+	log_pmf = log_equal_;
+      else {
+	if (rep_diff < 0)
+	  log_pmf = in_log_down_ + in_log_nostep_ + in_log_step_*(-rep_diff-1);
+	else
+	  log_pmf = in_log_up_   + in_log_nostep_ + in_log_step_*(rep_diff-1);
+      }
     }
+    assert(log_pmf <= 0);
+    return log_pmf;
   }
 
 
@@ -119,11 +128,11 @@ class StutterModel {
     int next_rep_diff = (min_bp_diff < 0 || min_bp_diff % motif_len_ == 0 ? min_bp_diff/motif_len_:  1 + min_bp_diff/motif_len_);
     if (next_rep_diff < 0){
       log_probs.push_back(in_log_down_ + log_geom_leq(in_geom_, -next_rep_diff));
-      log_probs.push_back(in_log_equal_);
+      log_probs.push_back(log_equal_);
       log_probs.push_back(in_log_up_);    
     }
     else if (next_rep_diff == 0){
-      log_probs.push_back(in_log_equal_);
+      log_probs.push_back(log_equal_);
       log_probs.push_back(in_log_up_);
     }
     else
