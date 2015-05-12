892bf132615775a494e58b783691e16954da2cba
Thomas Willems <twillems@club.nygenome.org>
2015-05-11 22:37:24 -0400

    Modified stutter insertion alignment to incorporate match to surrounding sequence

--- SeqAlignment/StutterAligner.cpp
+++ SeqAlignment/StutterAligner.cpp
@@ -32,14 +32,15 @@ double align_pcr_insertion_reverse(int block_len,                const char*   b
 				   int base_seq_len,             const char*   base_seq,
 				   const double* base_log_wrong, const double* base_log_correct,
 				   int D, int period){
-  assert(D > 0 && base_seq_len <= block_len+D);
+  assert(D > 0 && base_seq_len <= block_len+D && D%period == 0);
   std::vector<double> log_probs; log_probs.reserve(block_len+1);
   double log_prior = log(1.0/(block_len+1));
 
   // Compute probability for i = 0
   double log_prob = log_prior;
+  // Bases matched with insertion. Calculate emission probability according to agreement with proximal haplotype sequence
   for (int j = 0; j < std::min(D, base_seq_len); j++)
-    log_prob += base_log_correct[-j]; // Bases matched with insertion
+    log_prob += (base_seq[-j] == block_seq[-(j%period)] ? base_log_correct[-j] : base_log_wrong[-j]);
   for (int j = D; j < base_seq_len; j++)
     log_prob += (block_seq[-j+D] == base_seq[-j] ? base_log_correct[-j] : base_log_wrong[-j]); // Bases matched with block characters
   log_probs.push_back(log_prob);
@@ -47,31 +48,20 @@ double align_pcr_insertion_reverse(int block_len,                const char*   b
   // Compute for all other i's, reusing previous result to accelerate computation
   int i = 1;
   for (; i <= std::min(std::max(0, base_seq_len-D), block_len); i++){
-    // Update term for bases matched to insertions
-    log_prob -= base_log_correct[-i+1];
-    log_prob += base_log_correct[-i-D+1];
-    
-    // Update term for bases matched to stutter block
-    log_prob -= (block_seq[-i+1] == base_seq[-i-D+1] ? base_log_correct[-i-D+1] : base_log_wrong[-i-D+1]);
-    log_prob += (block_seq[-i+1] == base_seq[-i+1]   ? base_log_correct[-i+1]   : base_log_wrong[-i+1]);
-
-    // Store LL for configuration
-    log_probs.push_back(log_prob);
-  }
-
-  // Process remaining configurations 
-  if (base_seq_len < block_len+D){
-    while (i <= base_seq_len && i <= block_len){
-      log_prob -= base_log_correct[-i+1];
-      log_prob += (block_seq[-i+1] == base_seq[-i+1] ? base_log_correct[-i+1] : base_log_wrong[-i+1]);
-      log_probs.push_back(log_prob);
-      i++;
+    if (i+period-1 < block_len) {
+      if (block_seq[-i+1] != block_seq[-i-period+1]){
+        for (int index = -i-period+1; index >= -i-D+1; index -= period){
+          log_prob -= (base_seq[index] == block_seq[-i+1]        ? base_log_correct[index] : base_log_wrong[index]);
+          log_prob += (base_seq[index] == block_seq[-i-period+1] ? base_log_correct[index] : base_log_wrong[index]);
+        }
+      }
     }
-
-    // Remaining configurations all have same likelihood so count all of them
-    if (i <= block_len)
-      log_probs.push_back(log(block_len+1-i)+log_prob);
+    log_probs.push_back(log_prob); // Store LL for configuration
   }
+
+  // Remaining configurations all have same likelihood so count all of them
+  if (base_seq_len < block_len+D)
+    log_probs.push_back(log(block_len+1-i)+log_prob);
 
   // Convert to raw probabilities, add, take the log while avoiding underflow
   return fast_log_sum_exp(log_probs);
@@ -81,46 +71,37 @@ double align_pcr_insertion_forward(int block_len,                const char*   b
 				   int base_seq_len,             const char*   base_seq,
 				   const double* base_log_wrong, const double* base_log_correct,
 				   int D, int period){
-  assert(D > 0 && base_seq_len <= block_len+D);
+  assert(D > 0 && base_seq_len <= block_len+D && D%period == 0);
   std::vector<double> log_probs; log_probs.reserve(block_len+1);
   double log_prior = log(1.0/(block_len+1));
 
   // Compute probability for i = 0
   double log_prob = log_prior;
+  // Bases matched with insertion. Calculate emission probability according to agreement with proximal haplotype sequence
   for (int j = 0; j < std::min(D, base_seq_len); j++)
-    log_prob += base_log_correct[j]; // Bases matched with insertion
+    log_prob += (base_seq[j] == block_seq[j%period] ? base_log_correct[j] : base_log_wrong[j]);
+  // Bases matched with block characters
   for (int j = D; j < base_seq_len; j++)
-    log_prob += (block_seq[j-D] == base_seq[j] ? base_log_correct[j] : base_log_wrong[j]); // Bases matched with block characters
+    log_prob += (block_seq[j-D] == base_seq[j] ? base_log_correct[j] : base_log_wrong[j]);
   log_probs.push_back(log_prob);
 
   // Compute for all other i's, reusing previous result to accelerate computation
   int i = 1;
   for (; i <= std::min(std::max(0, base_seq_len-D), block_len); i++){
-    // Update term for bases matched to insertions
-    log_prob -= base_log_correct[i-1];
-    log_prob += base_log_correct[i+D-1];
-    
-    // Update term for bases matched to stutter block
-    log_prob -= (block_seq[i-1] == base_seq[i+D-1] ? base_log_correct[i+D-1] : base_log_wrong[i+D-1]);
-    log_prob += (block_seq[i-1] == base_seq[i-1]   ? base_log_correct[i-1]   : base_log_wrong[i-1]);
-
-    // Store LL for configuration
-    log_probs.push_back(log_prob);
-  }
-
-  // Process remaining configurations 
-  if(base_seq_len < block_len+D){
-    while (i <= base_seq_len && i <= block_len){
-      log_prob -= base_log_correct[i-1];
-      log_prob += (block_seq[i-1] == base_seq[i-1] ? base_log_correct[i-1] : base_log_wrong[i-1]);
-      log_probs.push_back(log_prob);
-      i++;
+    if (i+period-1 < block_len) {
+      if (block_seq[i-1] != block_seq[i+period-1]){
+	for (int index = i+period-1; index <= i+D-1; index += period){
+	  log_prob -= (base_seq[index] == block_seq[i-1]        ? base_log_correct[index] : base_log_wrong[index]);
+	  log_prob += (base_seq[index] == block_seq[i+period-1] ? base_log_correct[index] : base_log_wrong[index]);
+	}
+      }
     }
-
-    // Remaining configurations all have same likelihood so count all of them
-    if (i <= block_len)
-      log_probs.push_back(log(block_len+1-i)+log_prob);
+    log_probs.push_back(log_prob); // Store LL for configuration
   }
+  
+  // Remaining configurations all have same likelihood so count all of them
+  if (base_seq_len < block_len+D)
+    log_probs.push_back(log(block_len+1-i)+log_prob);
 
   // Convert to raw probabilities, add, take the log while avoiding underflow
   return fast_log_sum_exp(log_probs);
@@ -209,3 +190,4 @@ double align_stutter_region_forward(int block_len,                const char*   
   else
     return align_pcr_deletion_forward(block_len, block_seq, base_seq_len, base_seq, base_log_wrong, base_log_correct, D);
 }
+
