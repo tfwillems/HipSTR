d1d36e05a66555f0a287556fa59f4632b80a1322
Thomas Willems <twillems@club.nygenome.org>
2015-05-17 16:12:38 -0400

    Simplify loop indexing

--- SeqAlignment/StutterAligner.cpp
+++ SeqAlignment/StutterAligner.cpp
@@ -46,13 +46,13 @@ double align_pcr_insertion_reverse(int block_len,                const char*   b
   log_probs.push_back(log_prob);
 
   // Compute for all other i's, reusing previous result to accelerate computation
-  int i = 1;
-  for (; i <= std::min(std::max(0, base_seq_len-D), block_len); i++){
-    if (i+period-1 < block_len) {
-      if (block_seq[-i+1] != block_seq[-i-period+1]){
-        for (int index = -i-period+1; index >= -i-D+1; index -= period){
-          log_prob -= (base_seq[index] == block_seq[-i+1]        ? base_log_correct[index] : base_log_wrong[index]);
-          log_prob += (base_seq[index] == block_seq[-i-period+1] ? base_log_correct[index] : base_log_wrong[index]);
+  int i = 0;
+  for (; i > -std::min(std::max(0, base_seq_len-D), block_len); i--){
+    if (-i+period < block_len) {
+      if (block_seq[i] != block_seq[i-period]){
+        for (int index = i-period; index >= i-D; index -= period){
+          log_prob -= (base_seq[index] == block_seq[i]        ? base_log_correct[index] : base_log_wrong[index]);
+          log_prob += (base_seq[index] == block_seq[i-period] ? base_log_correct[index] : base_log_wrong[index]);
         }
       }
     }
@@ -61,7 +61,7 @@ double align_pcr_insertion_reverse(int block_len,                const char*   b
 
   // Remaining configurations all have same likelihood so count all of them
   if (base_seq_len < block_len+D)
-    log_probs.push_back(log(block_len+1-i)+log_prob);
+    log_probs.push_back(log(block_len+i)+log_prob);
 
   // Convert to raw probabilities, add, take the log while avoiding underflow
   return fast_log_sum_exp(log_probs);
@@ -86,13 +86,13 @@ double align_pcr_insertion_forward(int block_len,                const char*   b
   log_probs.push_back(log_prob);
 
   // Compute for all other i's, reusing previous result to accelerate computation
-  int i = 1;
-  for (; i <= std::min(std::max(0, base_seq_len-D), block_len); i++){
-    if (i+period-1 < block_len) {
-      if (block_seq[i-1] != block_seq[i+period-1]){
-	for (int index = i+period-1; index <= i+D-1; index += period){
-	  log_prob -= (base_seq[index] == block_seq[i-1]        ? base_log_correct[index] : base_log_wrong[index]);
-	  log_prob += (base_seq[index] == block_seq[i+period-1] ? base_log_correct[index] : base_log_wrong[index]);
+  int i = 0;
+  for (; i < std::min(std::max(0, base_seq_len-D), block_len); i++){
+    if (i+period < block_len) {
+      if (block_seq[i] != block_seq[i+period]){
+	for (int index = i+period; index <= i+D; index += period){
+	  log_prob -= (base_seq[index] == block_seq[i]        ? base_log_correct[index] : base_log_wrong[index]);
+	  log_prob += (base_seq[index] == block_seq[i+period] ? base_log_correct[index] : base_log_wrong[index]);
 	}
       }
     }
@@ -101,7 +101,7 @@ double align_pcr_insertion_forward(int block_len,                const char*   b
   
   // Remaining configurations all have same likelihood so count all of them
   if (base_seq_len < block_len+D)
-    log_probs.push_back(log(block_len+1-i)+log_prob);
+    log_probs.push_back(log(block_len-i)+log_prob);
 
   // Convert to raw probabilities, add, take the log while avoiding underflow
   return fast_log_sum_exp(log_probs);
@@ -117,14 +117,14 @@ double align_pcr_deletion_reverse(int block_len,                const char*   bl
 
   // Compute probability for i = 0
   double log_prob = log_prior;
-  for (int j = 0; j < base_seq_len; j++)
-    log_prob += (block_seq[-j+D] == base_seq[-j] ? base_log_correct[-j] : base_log_wrong[-j]);
+  for (int j = 0; j > -base_seq_len; j--)
+    log_prob += (block_seq[j+D] == base_seq[j] ? base_log_correct[j] : base_log_wrong[j]);
   log_probs.push_back(log_prob);
 
   // Compute for all other i's, reusing previous result to accelerate computation
-  for (int i = 1; i <= base_seq_len; i++){
-    log_prob    -= (block_seq[-i+1+D] == base_seq[-i+1] ? base_log_correct[-i+1] : base_log_wrong[-i+1]);
-    log_prob    += (block_seq[-i+1]   == base_seq[-i+1] ? base_log_correct[-i+1] : base_log_wrong[-i+1]);
+  for (int i = 0; i > -base_seq_len; i--){
+    log_prob    -= (block_seq[i+D] == base_seq[i] ? base_log_correct[i] : base_log_wrong[i]);
+    log_prob    += (block_seq[i]   == base_seq[i] ? base_log_correct[i] : base_log_wrong[i]);
     log_probs.push_back(log_prob);
   }
 
@@ -151,9 +151,9 @@ double align_pcr_deletion_forward(int block_len,                const char*   bl
   log_probs.push_back(log_prob);
 
   // Compute for all other i's, reusing previous result to accelerate computation
-  for (int i = 1; i <= base_seq_len; i++){
-    log_prob    -= (block_seq[i-1-D] == base_seq[i-1] ? base_log_correct[i-1] : base_log_wrong[i-1]);
-    log_prob    += (block_seq[i-1]   == base_seq[i-1] ? base_log_correct[i-1] : base_log_wrong[i-1]);
+  for (int i = 0; i < base_seq_len; i++){
+    log_prob    -= (block_seq[i-D] == base_seq[i] ? base_log_correct[i] : base_log_wrong[i]);
+    log_prob    += (block_seq[i]   == base_seq[i] ? base_log_correct[i] : base_log_wrong[i]);
     log_probs.push_back(log_prob);
   }
 
@@ -164,8 +164,6 @@ double align_pcr_deletion_forward(int block_len,                const char*   bl
   // Convert to raw probabilities, add, take the log while avoiding underflow
   return fast_log_sum_exp(log_probs);
 }
-
-
 
 double align_stutter_region_reverse(int block_len,                const char*   block_seq,
 				    int base_seq_len,             const char*   base_seq,
