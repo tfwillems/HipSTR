acb2c3938786b6b8d730b6cd112707bcecece1ed
Thomas Willems <twillems@club.nygenome.org>
2015-06-25 00:05:39 -0400

    Trim reference sequence to only pad 10bp around read s

--- filter_html_alns.py
+++ filter_html_alns.py
@@ -12,7 +12,7 @@ from HTMLParser import HTMLParser
 
 class HTMLCharCounter(HTMLParser):
     def __init__(self):
-        self.counts = collections.defaultdict(int)
+        self.counts = {}
         HTMLParser.__init__(self)
         
     def handle_starttag(self, tag, attrs):
@@ -31,12 +31,13 @@ class HTMLCharCounter(HTMLParser):
         if data == ' ':
             return
         if self.coord not in self.counts:
-            self.counts[self.coord] = set()
-        self.counts[self.coord].add(data.upper())
+            self.counts[self.coord] = collections.defaultdict(int)
+        self.counts[self.coord][data.upper()] += 1
 
 class FilteredHTMLOutputter(HTMLParser):
-    def __init__(self, skip_columns):
+    def __init__(self, skip_columns, left_trim):
         self.skip_columns = skip_columns
+        self.left_trim    = left_trim
         HTMLParser.__init__(self)
         
     def handle_starttag(self, tag, attrs):
@@ -46,7 +47,11 @@ class FilteredHTMLOutputter(HTMLParser):
         output       = "<" + tag 
         coord_change = 1
         for i in xrange(len(attrs)):
-            output += " %s=\"%s\""%(attrs[i][0], attrs[i][1])
+            if attrs[i][0] == "colspan":
+                output += " %s=\"%d\""%(attrs[i][0], int(attrs[i][1])-self.left_trim)
+            else:
+                output += " %s=\"%s\""%(attrs[i][0], attrs[i][1])
+
             if tag == "td":
                 if attrs[i][0] == "colspan":
                     coord_change = int(attrs[i][1])
@@ -75,18 +80,43 @@ def main():
     # Read all lines in first pass and determine which columns
     # only have a * character (insertion)
     parser = HTMLCharCounter()
+    nlines = 0
     for line in sys.stdin:
         lines.append(line)
         if not line.startswith("<tr") or "samplename" in line:
             continue
         parser.feed(line.strip())
+        nlines += 1
     bad_cols = set()
     for key, vals in parser.counts.items():
         if len(vals) == 1 and "*" in vals:
             bad_cols.add(key)
 
+    if nlines != 1:
+        fixed_points = map(lambda x: x[0], sorted(filter(lambda x: len(x[1]) == 1 and (x[1].values()[0] == 1), parser.counts.items())))
+        if len(fixed_points) >= 1 and fixed_points[0] == 1:
+            trim_index = 1
+            while trim_index < len(fixed_points) and fixed_points[trim_index] == fixed_points[trim_index-1]+1:
+                trim_index += 1
+            trim_index = max(0, trim_index-10)
+            for i in xrange(0, trim_index):
+                bad_cols.add(fixed_points[i])
+            left_trim = 0 if trim_index == 0 else fixed_points[trim_index-1]
+        else:
+            left_trim = 0
+
+        if len(fixed_points) >= 1 and fixed_points[-1] == sorted(parser.counts.keys())[-1]:
+            trim_index = len(fixed_points)-2
+            while trim_index >= 0 and fixed_points[trim_index]+1 == fixed_points[trim_index+1]:
+                trim_index -= 1
+            trim_index = min(len(fixed_points), trim_index+10)
+            for i in xrange(trim_index, len(fixed_points)):
+                bad_cols.add(fixed_points[i])
+    else:
+        left_trim = 0
+
     # Output each line after removing columns only containing a *
-    output = FilteredHTMLOutputter(bad_cols)
+    output = FilteredHTMLOutputter(bad_cols, left_trim)
     for line in lines:
         if not line.startswith("<tr") or "samplename" in line:
             sys.stdout.write(line)
