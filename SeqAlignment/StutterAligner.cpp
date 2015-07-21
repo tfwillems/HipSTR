5ce6e73e599b0872864eb52468866282a562d38c
Thomas Willems <twillems@club.nygenome.org>
2015-07-21 13:52:57 -0400

    Extending functions to track the best indel positions

--- SeqAlignment/StutterAligner.cpp
+++ SeqAlignment/StutterAligner.cpp
@@ -21,7 +21,8 @@ double align_no_artifact_reverse(int block_len,                const char*   blo
 double align_pcr_insertion_reverse(int block_len,                const char*   block_seq,
 				   int base_seq_len,             const char*   base_seq,
 				   const double* base_log_wrong, const double* base_log_correct,
-				   int D, int period){
+				   bool left_align, int D, int period,
+				   int& best_ins_pos){
   assert(D > 0 && base_seq_len <= block_len+D && D%period == 0);
   std::vector<double> log_probs; log_probs.reserve(block_len+1);
   double log_prior = log(1.0/(block_len+1));
@@ -34,6 +35,7 @@ double align_pcr_insertion_reverse(int block_len,                const char*   b
   for (int j = D; j < base_seq_len; j++)
     log_prob += (block_seq[-j+D] == base_seq[-j] ? base_log_correct[-j] : base_log_wrong[-j]); // Bases matched with block characters
   log_probs.push_back(log_prob);
+  best_ins_pos = 0;
 
   // Compute for all other i's, reusing previous result to accelerate computation
   int i = 0;
@@ -47,6 +49,8 @@ double align_pcr_insertion_reverse(int block_len,                const char*   b
       }
     }
     log_probs.push_back(log_prob); // Store LL for configuration
+    if (log_prob > log_probs[best_ins_pos] || (left_align && (log_prob == log_probs[best_ins_pos])))
+      best_ins_pos = log_probs.size()-1;
   }
 
   // Remaining configurations all have same likelihood so count all of them
@@ -59,8 +63,8 @@ double align_pcr_insertion_reverse(int block_len,                const char*   b
 
 double align_pcr_deletion_reverse(int block_len,                const char*   block_seq,
 				  int base_seq_len,             const char*   base_seq,
-				  const double* base_log_wrong, const double* base_log_correct,
-				  int D){
+				  const double* base_log_wrong, const double* base_log_correct, bool left_align, int D,
+				  int& best_del_pos){
   assert(D < 0 && block_len+D >= 0 && base_seq_len <= block_len+D);
   std::vector<double> log_probs; log_probs.reserve(block_len+D+1);
   double log_prior = log(1.0/(block_len+D+1));
@@ -70,12 +74,15 @@ double align_pcr_deletion_reverse(int block_len,                const char*   bl
   for (int j = 0; j > -base_seq_len; j--)
     log_prob += (block_seq[j+D] == base_seq[j] ? base_log_correct[j] : base_log_wrong[j]);
   log_probs.push_back(log_prob);
+  best_del_pos = 0;
 
   // Compute for all other i's, reusing previous result to accelerate computation
   for (int i = 0; i > -base_seq_len; i--){
     log_prob    -= (block_seq[i+D] == base_seq[i] ? base_log_correct[i] : base_log_wrong[i]);
     log_prob    += (block_seq[i]   == base_seq[i] ? base_log_correct[i] : base_log_wrong[i]);
     log_probs.push_back(log_prob);
+    if (log_prob > log_probs[best_del_pos] || (left_align && (log_prob == log_probs[best_del_pos])))
+      best_del_pos = log_probs.size()-1;
   }
 
   // Remaining configurations all have same likelihood so count all of them
@@ -89,11 +96,13 @@ double align_pcr_deletion_reverse(int block_len,                const char*   bl
 double align_stutter_region_reverse(int block_len,                const char*   block_seq,
 				    int base_seq_len,             const char*   base_seq,
 				    const double* base_log_wrong, const double* base_log_correct,
-				    int D, int period){
+				    bool left_align, int D, int period,
+				    int& best_pos){
+  best_pos = -1;
   if (D == 0)
     return align_no_artifact_reverse(block_len, block_seq, base_seq_len, base_seq, base_log_wrong, base_log_correct);
   else if (D > 0)
-    return align_pcr_insertion_reverse(block_len, block_seq, base_seq_len, base_seq, base_log_wrong, base_log_correct, D, period);
+    return align_pcr_insertion_reverse(block_len, block_seq, base_seq_len, base_seq, base_log_wrong, base_log_correct, left_align, D, period, best_pos);
   else
-    return align_pcr_deletion_reverse(block_len, block_seq, base_seq_len, base_seq, base_log_wrong, base_log_correct, D);
+    return align_pcr_deletion_reverse(block_len, block_seq, base_seq_len, base_seq, base_log_wrong, base_log_correct, left_align, D, best_pos);
 }
--- SeqAlignment/StutterAligner.h
+++ SeqAlignment/StutterAligner.h
@@ -6,14 +6,18 @@
 double align_no_artifact_reverse(int block_len,                const char*   block_seq,
                                  int base_seq_len,             const char*   base_seq,
                                  const double* base_log_wrong, const double* base_log_correct);
+
 double align_pcr_insertion_reverse(int block_len,                const char*   block_seq,
                                    int base_seq_len,             const char*   base_seq,
                                    const double* base_log_wrong, const double* base_log_correct,
-                                   int D, int period);
+                                   int D, int period,
+				   int& best_ins_pos);
+
 double align_pcr_deletion_reverse(int block_len,                const char*   block_seq,
                                   int base_seq_len,             const char*   base_seq,
                                   const double* base_log_wrong, const double* base_log_correct,
-                                  int D);
+                                  int D,
+				  int& best_del_pos);
 
 /* Returns the total log-likelihood of the base sequence given the block sequence and the associated quality scores.
  * Assumes that an artifact of size D occurs with equal probability throughout the block sequence.
@@ -23,7 +27,8 @@ double align_pcr_deletion_reverse(int block_len,                const char*   bl
 double align_stutter_region_reverse(int block_len,                const char*   block_seq,
 				    int base_seq_len,             const char*   base_seq,
 				    const double* base_log_wrong, const double* base_log_correct,
-				    int D, int period);
+				    bool left_align, int D, int period,
+				    int& best_pos);
 
 #endif
 
