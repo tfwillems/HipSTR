d078333c39609edf1ec28e829df864b6e68e0431
Thomas Willems <twillems@club.nygenome.org>
2015-11-04 12:36:24 -0500

    Created simple class to track times of processes

---
+++ process_timer.cpp
@@ -0,0 +1,1 @@
+#include "process_timer.h"
---
+++ process_timer.h
@@ -0,0 +1,29 @@
+#ifndef PROCESS_TIMER_H_
+#define PROCESS_TIMER_H_
+
+#include <map>
+#include <string>
+
+class ProcessTimer {
+ private:
+  std::map<std::string, double> total_times_;
+
+ public:
+  ProcessTimer(){}
+  
+  void add_time(std::string key, double time){
+    if (total_times_.find(key) == total_times_.end())
+      total_times_[key] = 0.0;
+    total_times_[key] += time;
+  }
+
+  double get_total_time(std::string key){
+    auto iter = total_times_.find(key);
+    if (iter == total_times_.end())
+      return 0.0;
+    return iter->second;
+  }
+};
+
+
+#endif
