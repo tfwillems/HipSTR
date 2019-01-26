37719cba53c79bc97138c5bd40a4f56a71da8e2c
Thomas Willems <twillems@mit.edu>
2019-01-26 12:54:27 -0500

    Remove traceback code from file. Will migrate to AlignmentState

--- src/SeqAlignment/AlignmentTraceback.h
+++ src/SeqAlignment/AlignmentTraceback.h
@@ -5,7 +5,6 @@
 #include <vector>
 
 #include "AlignmentData.h"
-#include "Haplotype.h"
 
 class AlignmentTrace {
  private:
@@ -106,12 +105,5 @@ class AlignmentTrace {
     return str_data_[block_index]->str_seq();
   }
 };
-
-
-std::string stitch(const std::string& hap_aln, const std::string& read_aln, int h_index, int r_index, int increment);
-
-void stitch_alignment_trace(int32_t hap_start, const std::string& hap_aln_to_ref, 
-			    const std::string& read_aln_to_hap, int hap_index, int seed_base, const Alignment& orig_aln,
-			    Alignment& new_aln);
 
 #endif
