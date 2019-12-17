12309e5108603c67f90663d6fe464c9eac4caec3
Thomas Willems <thomas_willems@vrtx.com>
2019-12-17 14:52:07 -0500

    Update test to reflect change to VCF input function

--- test/read_vcf_alleles_test.cpp
+++ test/read_vcf_alleles_test.cpp
@@ -18,20 +18,32 @@ int main(int argc, char* argv[]){
   readRegions(region_file, 1000, "", regions, std::cerr);
 
   VCF::VCFReader ref_vcf(vcf_file);
-
-  std::vector<std::string> alleles;
+  std::vector<std::string> alleles, lflanks, rflanks;
   int32_t pos;
   for (unsigned int i = 0; i < regions.size(); i++){
-    bool success = read_vcf_alleles(&ref_vcf, regions[i], alleles, pos);
+    bool success = read_vcf_alleles(&ref_vcf, regions[i], alleles, lflanks, rflanks, pos);
     if (success){
       std::cerr << "Position=" << pos << std::endl;
       std::cerr << "Alleles:" << std::endl;
       for (unsigned int j = 0; j < alleles.size(); j++)
 	std::cerr << alleles[j] << std::endl;
+
+      if (!lflanks.empty()){
+	std::cerr << "Left Flanks:" << std::endl;
+	for (unsigned int j = 0; j < lflanks.size(); j++)
+	  std::cerr << lflanks[j] << std::endl;
+      }
+
+      if (!rflanks.empty()){
+	std::cerr << "Right Flanks:" << std::endl;
+	for (unsigned int j = 0; j < rflanks.size(); j++)
+	  std::cerr << rflanks[j] << std::endl;
+      }
     }
     else {
       std::cerr << "Failed to read alleles for region " << regions[i].str() << std::endl; 
     }
     alleles.clear();
+    std::cerr << std::endl << std::endl;
   }
 }
